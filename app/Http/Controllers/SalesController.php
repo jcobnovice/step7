0a711fd8c9734b0bddf08635042457ce2fd8ded1
jcobnovice <jajeje753951@gmail.com>
2023-10-19 17:04:58 +0900

    SalesController Transaction

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -5,27 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Products;
 use App\Models\Sale;
+use Illuminate\Support\Facades\DB;
 
 class SalesController extends Controller
 {
     public function purchase(Request $request)
     {
-        $productId = $request->input('product_id');
-        $quantity = $request->input('quantity', 1);
+        // トランザクション開始
+        DB::beginTransaction();
 
-        $product = Products::find($productId);
+        try {
+            // 登録処理呼び出し
+            $productId = $request->input('product_id');
+            $quantity = $request->input('quantity', 1);
 
-        if (!$product) {
-            return response()->json(['message' => '商品が存在しません'], 404);
+            $product = Products::find($productId);
+
+            if (!$product) {
+                return response()->json(['message' => '商品が存在しません']);
+            }
+            if ($product->stock < $quantity) {
+                return response()->json(['message' => '商品が在庫不足です']);
+            }
+
+            $sales = new Sale();
+            $sales->productdec($productId);
+            DB::commit();
+
+        } catch (\Exception $e) {
+            DB::rollback();
         }
-        if ($product->stock < $quantity) {
-            return response()->json(['message' => '商品が在庫不足です'], 400);
-        }
-
-        $sales = new Sale();
-        $result = $sales->productdec($productId);
-
-        return $result;
+        
         return response()->json(['message' => '購入成功']);
     }
 }
